1b368c96a4b5f2df357a8e99656f96d8c58d7a2d
Ilya <ilya.olegovich.kovalenko@gmail.com>
2018-10-28 14:45:36 +0300

    add lab4

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Set
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
